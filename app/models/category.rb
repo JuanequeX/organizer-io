95c497ed92d229b37eb2cf7fb53f1544409fb0e0
Juanequex <ramon.g.uz37@gmail.com>
2022-01-27 23:28:08 -0600

    Validations on models

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,4 +10,9 @@
 #
 class Category < ApplicationRecord
   has_many :tasks
+  # Con esto no permitiremos registrar campos vacios
+  validates :name, :description, presence: true
+  # Podremos validar que el nombre sea unico y tomando en cuenta
+  # Mayusculas y minusculas
+  validates :name, uniqueness: { case_sensitive: false }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -12,4 +12,23 @@
 #
 class Task < ApplicationRecord
   belongs_to :category
+
+  validates :name, :description, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
+  # Validacion perzonalizada para que la fecha no se encuentre
+  # en una fecha que este en el pasado.
+  validate :due_date_validity
+  # Esta validacion debe de ser acompanada de un metodo:
+
+  def due_date_validity
+    # Primero validaremos que la variable existe
+    return if due_date.blank?
+    return if due_date > Date.today
+    # Usaremos el metodo interno errors para añadir
+    # Errores al modelo y asi hacerle saber al usuario
+    # Que no puede agregar una fecha fuera de estas validaciones
+    # Usaremos la internacionalizacion pero como no podemos usar directamente
+    # el helper 't' lo hacemos asi:
+    errors.add :due_date, I18n.t('task.errors.invalid_due_date')
+  end
 end
