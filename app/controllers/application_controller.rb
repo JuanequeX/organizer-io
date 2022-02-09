dcddb21ec65c9a72f76520ea1c9ff395e2ba6e7b
Juanequex <ramon.g.uz37@gmail.com>
2022-02-09 13:40:39 -0600

    Update project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+  # Con esto permitiremos que solo usuarios
+  # Logeados puedan acceder a nuestras vistas
+  # de task y categories.
+  before_action  :authenticate_user!
+
   def set_locale
     I18n.locale = 'es'
   end
